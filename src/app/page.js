47020a379797cb15d16b8cbd95a4136427b61190
Sandeep Ghosh <mailsg0912@gmail.com>
2024-03-03 17:40:57 +0530

    Fix the UI of the App

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import StoreProvider from "./StoreProvider";
-import Image from "next/image";
 import TaskForm from "@/components/TaskForm";
 import TaskList from "@/components/TaskList";
 
@@ -14,123 +13,58 @@ export default function Home() {
   };
 
   return (
-    <StoreProvider>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            Task Manager - &nbsp;
-            <code className="font-mono font-bold">Version 1.0</code>
+<StoreProvider>
+      <main className="min-h-screen flex flex-col justify-between p-4 lg:p-24">
+        <div className="max-w-5xl mx-auto w-full">
+          <p className="mb-8 text-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 lg:text-left lg:pt-0 lg:border-none lg:bg-none">
+            Task Manager - <code className="font-bold">Version 1.0</code>
           </p>
-          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-            <a
-              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-              href="https://my-portfolio-3fbn.onrender.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {/* By {" "} */}
-              <Image
-                src="/logo.png"
-                alt="My Logo"
-                className="dark:invert"
-                width={75}
-                height={25}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className="relative flex place-items-center">
-
-            <div className="flex flex-col left-10 space-y-4">
-
-            {showTaskForm ? (
-              <TaskForm />
-            ) : (
-              <>
-                <div className="h-96 overflow-auto">
-                  <TaskList />
-                </div>
-              </>
-            )}
-
+          <div className="flex flex-col items-center">
+            {showTaskForm ? <TaskForm /> : <TaskList />}
             <button
               onClick={toggleTaskForm}
-              className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
             >
               {showTaskForm ? 'Show task list' : 'Add a new task'}
             </button>
           </div>
         </div>
-
-        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <div className="grid text-center mt-8 lg:max-w-5xl lg:grid-cols-4 ml-1 lg:ml-20">
           <a
             href="https://github.com/mailsg"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              GitHub{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Find my projects on GitHub.
-            </p>
+            <h2 className="mb-3 text-2xl text-center font-semibold">GitHub</h2>
+            <p className="max-w-[30ch] text-center ml-5 text-sm opacity-50">Find my projects on GitHub.</p>
           </a>
-
           <a
             href="https://my-portfolio-3fbn.onrender.com/"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Portfolio{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Checkout my portfolio!
-            </p>
+            <h2 className="mb-3 text-2xl text-center font-semibold">Portfolio</h2>
+            <p className="max-w-[30ch] text-center ml-5 text-sm opacity-50">Checkout my portfolio!</p>
           </a>
-
           <a
             href="https://www.linkedin.com/in/sandeep0912/"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              LinkedIn{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Connect with me on LinkedIn.
-            </p>
+            <h2 className="mb-3 text-2xl text-center font-semibold">LinkedIn</h2>
+            <p className="max-w-[30ch] text-center ml-5 text-sm opacity-50">Connect on LinkedIn.</p>
           </a>
-
           <a
             href="https://wellfound.com/u/sandeep0912"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Wellfound{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-              My Wellfound profile.
-            </p>
+            <h2 className="mb-3 text-2xl text-center font-semibold">Wellfound</h2>
+            <p className="max-w-[30ch] text-center ml-5 text-sm opacity-50">My Wellfound profile.</p>
           </a>
         </div>
       </main>
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,10 @@
 "use client";
-import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import { fetchTasks, updateTask, deleteTask } from "@/redux/tasks/slice/tasksSlice";
 import FilterDropDown from "./FilterDropDown";
 
 const TaskList = () => {
-
   const dispatch = useDispatch();
   const tasks = useSelector((state) => state.tasks.tasks);
   const isLoading = useSelector((state) => state.tasks.isLoading);
@@ -17,38 +16,32 @@ const TaskList = () => {
   const indexOfFirstTask = indexOfLastTask - tasksPerPage;
   const currentTasks = tasks.slice(indexOfFirstTask, indexOfLastTask);
   const totalPages = Math.ceil(tasks.length / tasksPerPage);
-  const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
 
-  const [selectedStatus, setSelectedStatus] = useState({});
-  const [selectedTaskId, setSelectedTaskId] = useState(null);
-  const [forceRender, setForceRender] = useState(false);
+  const [selectedStatus, setSelectedStatus] = useState("");
   const [filteredTasks, setFilteredTasks] = useState([]);
 
   useEffect(() => {
     dispatch(fetchTasks());
-  }, [ dispatch, forceRender]);
+  }, [dispatch]);
 
   useEffect(() => {
-        setFilteredTasks(tasks);
-}, [tasks]);
+    setFilteredTasks(tasks);
+    setCurrentPage(1);
+  }, [tasks]);
 
   const handleStatusChange = (event, taskId) => {
-    setSelectedStatus({ ...selectedStatus, [taskId]: event.target.value });
-    setSelectedTaskId(taskId);
+    const status = event.target.value;
+    setSelectedStatus({ ...selectedStatus, [taskId]: status });
   };
 
-  const handleSaveStatus = () => {
-    if (selectedTaskId && selectedStatus[selectedTaskId]) {
-      dispatch(updateTask({ taskId: selectedTaskId, updatedTask: { status: selectedStatus[selectedTaskId] } }))
-      .then(() => {
-        setSelectedStatus({ ...selectedStatus, [selectedTaskId]: null });
-        setSelectedTaskId(null);        
-      })
-      .then(() => setForceRender(prev => !prev));      
-    }
-  };  
+  const handleSaveStatus = (taskId) => {
+    const taskToUpdate = filteredTasks.find((task) => task._id === taskId);
+    if (!taskToUpdate || !selectedStatus[taskId]) return;
+    dispatch(updateTask({ taskId, updatedTask: { status: selectedStatus[taskId] } })).then(() => {
+      setSelectedStatus((prevStatus) => ({ ...prevStatus, [taskId]: null }));
+      dispatch(fetchTasks());
+    });
+  };
 
   const handleDeleteTask = (taskId) => {
     dispatch(deleteTask({ taskId }));
@@ -56,91 +49,88 @@ const TaskList = () => {
 
   const handleFilterChange = (status) => {
     setSelectedStatus(status);
-        if (status) {
-            const filtered = tasks.filter(task => task.status === status);
-            setFilteredTasks(filtered);
-        } else {
-            setFilteredTasks(tasks);
-        }
-  }
+    const filtered = status ? tasks.filter((task) => task.status === status) : tasks;
+    setFilteredTasks(filtered);
+    setCurrentPage(1);
+  };
 
   return (
-    <div>
-        <FilterDropDown onFilterChange={handleFilterChange} statuses={['To Do', 'In Progress', 'Done']}/>
+    <div className="max-w-4xl mx-auto px-4">
+      <FilterDropDown onFilterChange={handleFilterChange} statuses={["To Do", "In Progress", "Done"]} />
+      <div className="overflow-x-auto mt-4">
         <table className="min-w-full divide-y divide-gray-200">
-        <thead className="bg-gray-50">
+          <thead className="bg-gray-50">
             <tr>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 Title
-            </th>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              </th>
+              <th scope="col" className="hidden lg:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 Description
-            </th>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              </th>
+              <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 Status
-            </th>
-            <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              </th>
+              <th scope="col" className="hidden lg:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                 Actions
-            </th>
+              </th>
             </tr>
-        </thead>
-        <tbody className="bg-white divide-y divide-gray-200">
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
             {filteredTasks.slice(indexOfFirstTask, indexOfLastTask).map((task) => (
-              
               <tr key={task._id}>
                 <td className="px-6 py-4 whitespace-nowrap">{task.title}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{task.description}</td>
+                <td className="hidden lg:table-cell px-6 py-4 whitespace-nowrap">{task.description}</td>
                 <td className="px-6 py-4 whitespace-nowrap">
-
-                {/* <div className="flex items-center space-x-2"> */}
                   <select
                     value={selectedStatus[task._id] || task.status}
                     onChange={(e) => handleStatusChange(e, task._id)}
                     className="p-1 border border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500"
                   >
-                    <option value="">To Do</option>
+                    <option value="To Do">To Do</option>
                     <option value="In Progress">In Progress</option>
                     <option value="Done">Done</option>
                   </select>
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap">
+                </td>
+                <td className="hidden lg:table-cell px-6 py-4 whitespace-nowrap">
                   <button
-                    onClick={handleSaveStatus}
+                    onClick={() => handleSaveStatus(task._id)}
                     disabled={!selectedStatus[task._id]}
                     className={`${
-                      !selectedStatus[task._id] ? 'opacity-50 cursor-not-allowed' : ''
+                      !selectedStatus[task._id] ? "opacity-50 cursor-not-allowed" : ""
                     } inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
                   >
                     Save
                   </button>
-
-                <button 
-                  onClick={() => handleDeleteTask(task._id)} 
-                  className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+                  <button
+                    onClick={() => handleDeleteTask(task._id)}
+                    className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                  >
                     Delete
-                </button>
-                {/* </div> */}
+                  </button>
                 </td>
-            </tr> 
-          ))}
-        </tbody>
-        </table>
-        <nav className="flex justify-center mt-4">
-            <ul className="flex">
-            {Array.from({ length: totalPages }, (_, i) => (
-                <li key={i} className="page-item">
-                <button
-                    onClick={() => paginate(i + 1)}
-                    className={`${
-                    currentPage === i + 1 ? "bg-blue-500" : ""
-                    } text-black hover:bg-blue-400 px-4 py-2 border rounded-full mr-2`}
-                >
-                    {i + 1}
-                </button>
-                </li>
+              </tr>
             ))}
-            </ul>
-      </nav>
+          </tbody>
+        </table>
+      </div>
+      {filteredTasks.length > tasksPerPage && (
+        <nav className="flex justify-center mt-4">
+          <ul className="flex">
+            {Array.from({ length: totalPages }, (_, i) => (
+              <li key={i} className="page-item">
+                <button
+                  onClick={() => setCurrentPage(i + 1)}
+                  className={`${
+                    currentPage === i + 1 ? "bg-blue-500" : ""
+                  } text-black hover:bg-blue-400 px-4 py-2 border rounded-full mr-2`}
+                >
+                  {i + 1}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
     </div>
   );
 };
